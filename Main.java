20fdfa7235ad96c6da0f0b16aad46c1aee5b7552
owokaiser <101186844+owokaiser@users.noreply.github.com>
2022-03-22 12:40:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    double f;
+    double c;
+    double d;
+    d= 0.56;
+    System.out.println("F C");
+    for(f=-450;f<=136;f++){
+      c=d*(f-32);
+      System.out.println(f+" "+c);}
+  }
+}
